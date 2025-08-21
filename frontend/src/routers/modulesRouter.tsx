4f20edb2a7ed36727539df75b15c20ff923d300c
Speencer Pulido <contactspeencer@gmail.com>
2025-08-21 13:03:32 -0600

    Create modulesRouter.tsx

---
+++ frontend/src/routers/modulesRouter.tsx
@@ -0,0 +1,19 @@
+// routers/modulesRouter.tsx
+/**
+ * =====================================================
+ *  NAME    : modulesRouter.tsx
+ *  DESCRIPTION: example directory
+ * =====================================================
+ */
+
+// ROUTERS
+import HomeRouter from "../modules/home/homeRouter";
+
+// DEFAULT
+export default function ModulesRouter() {
+  return (
+    <>
+      <HomeRouter />
+    </>
+  );
+}
