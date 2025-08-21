4c1c85f2f0c24754be88d5059f7b562e6d94c5af
Speencer Pulido <contactspeencer@gmail.com>
2025-08-21 13:00:48 -0600

    Update utils.ts

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -9,6 +9,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// TAILWIND
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
