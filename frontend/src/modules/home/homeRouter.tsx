2003e06f7f0e6dcc0c44997079768883dc37cb6c
Speencer Pulido <contactspeencer@gmail.com>
2025-08-21 13:10:20 -0600

    Update homeRouter.tsx

--- frontend/src/modules/home/homeRouter.tsx
+++ frontend/src/modules/home/homeRouter.tsx
@@ -12,17 +12,19 @@ import { ThemeProvider, ThemeToggle } from "./ThemeToggle";
 // MODULE
 export default function HomeRouter() {
   return (
-    <ThemeProvider defaultTheme="light">
-      <div
-        style={{
-          backgroundColor: "var(--background)",
-          color: "var(--foreground)",
-          transition: "background-color 0.3s, color 0.3s",
-        }}
-      >
-        <ThemeToggle />
-        {/* NEW ROUTERS */}
-      </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider defaultTheme="light">
+        <div
+          style={{
+            backgroundColor: "var(--background)",
+            color: "var(--foreground)",
+            transition: "background-color 0.3s, color 0.3s",
+          }}
+        >
+          <ThemeToggle />
+          {/* NEW ROUTERS */}
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
